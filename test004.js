83cf846f31bd7f6e5e0a45a54e682cd64a2244dd
Saoki11111 <t.aoki11111@gmail.com>
2020-06-29 00:12:13 +0900

    サイコロの実装

---
+++ test004.js
@@ -0,0 +1,21 @@
+// 1 から 100 までの整数のどれかが ランダムで表示される
+// Math.ceil = 切り上げ(1.1 = 2, 1.2 = 2)
+// Math.random() = 0~1 までの数値を返す * ほしい数
+
+random = Math.ceil(Math.random()*100);
+console.log("サイコロの結果は" + random + "です！");
+
+// --- //
+
+// 1 ~ 5 = サイコロのは 大成功です！
+// 96 ~ 100 = サイコロのは 大失敗です！
+
+random = Math.ceil(Math.random()*100);
+message = "サイコロの結果は" + random; 
+if (random <= 5){
+  console.log(message + "成功です！");
+} else if (96 <= random && random <= 100){
+    console.log(message + "失敗です！");
+}
+
+console.log(message + "です！");
