9ec0d674d5e6d3ebd861065feec4f6b1abc3202b
Saoki11111 <t.aoki11111@gmail.com>
2020-07-01 09:38:53 +0900

    場所移動

---
+++ test/test07.js
@@ -0,0 +1,12 @@
+// 点数 0~100 が入った配列
+// 配列を受け取り、最高点を返す getMax(ary) を作る
+
+num = [];
+getMax = function(numbers){
+  var min=[0];
+  var max=[];
+  for(var count = 0; count < numbers.length; count++){}
+  if(numbers[count0] < min){}
+}
+
+console.log(getMax[67, 23, 99])
