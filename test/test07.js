1f7f7b593c44248d8717604f82e8b046eb1680df
Saoki11111 <t.aoki11111@gmail.com>
2020-07-02 09:49:19 +0900

    js 演習

--- test/test07.js
+++ test/test07.js
@@ -1,12 +1,39 @@
 // 点数 0~100 が入った配列
 // 配列を受け取り、最高点を返す getMax(ary) を作る
 
-num = [];
-getMax = function(numbers){
-  var min=[0];
-  var max=[];
-  for(var count = 0; count < numbers.length; count++){}
-  if(numbers[count0] < min){}
+getMax = function(ary){
+  var result = -1;
+  for(var count = 0; count < ary.length; count++){
+    if(result < ary[count]){
+      // もし result より大きい値なら result に入れる
+      result = ary[count];
+    }
+  }
+  return result;
 }
 
-console.log(getMax[67, 23, 99])
+console.log(getMax([67, 23, 99]))
+
+// -- //
+
+// テストの点数 0 ~ 100 の配列
+// min点、max点を受取り、
+// min点~max点を受け取った人数を返す
+// getRangeCount(ary, min, max) を作る
+// min点、max点をとった人数を含む
+
+// min < x < max = count 返す
+
+// 0~50点をとった人数は1人
+
+getRangeCount = function(ary, min, max) {
+  var count = 0;
+  for(var index = 0; index < ary.length; index++){
+    if(min <= ary[index] && ary[index] <= max){
+      count++;
+    }
+  }
+  return count;
+};
+
+console.log(getRangeCount([67, 23, 70] 0,50))
